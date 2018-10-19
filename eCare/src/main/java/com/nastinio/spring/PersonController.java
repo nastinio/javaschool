dc146b7961a0ae59e03a0ff80289671b45fa0135
nastinio <bessarabova.am@gmail.com>
2018-10-19 23:22:24 +0300

    Add template: springMVC, Hibernate

---
+++ eCare/src/main/java/com/nastinio/spring/PersonController.java
@@ -0,0 +1,67 @@
+package com.nastinio.spring;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.nastinio.spring.model.Person;
+import com.nastinio.spring.service.PersonService;
+
+@Controller
+public class PersonController {
+    /*
+    * controller class that will take care of client requests and uses service classes to perform database
+     * specific operations and then returns the view pages*/
+
+    private PersonService personService;
+
+    @Autowired(required=true)
+    @Qualifier(value="personService")
+    public void setPersonService(PersonService ps){
+        this.personService = ps;
+    }
+
+    @RequestMapping(value = "/persons", method = RequestMethod.GET)
+    public String listPersons(Model model) {
+        model.addAttribute("person", new Person());
+        model.addAttribute("listPersons", this.personService.listPersons());
+        return "person";
+    }
+
+    //For add and update person both
+    @RequestMapping(value= "/person/add", method = RequestMethod.POST)
+    public String addPerson(@ModelAttribute("person") Person p){
+
+        if(p.getId() == 0){
+            //new person, add it
+            this.personService.addPerson(p);
+        }else{
+            //existing person, call update
+            this.personService.updatePerson(p);
+        }
+
+        return "redirect:/persons";
+
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removePerson(@PathVariable("id") int id){
+
+        this.personService.removePerson(id);
+        return "redirect:/persons";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String editPerson(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", this.personService.getPersonById(id));
+        model.addAttribute("listPersons", this.personService.listPersons());
+        return "person";
+    }
+
+}
